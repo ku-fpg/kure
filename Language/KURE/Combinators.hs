7d32873e348c787dd35b736e8ad110c53dd30a41
Neil Sculthorpe <nas@ittc.ku.edu>
2012-04-06 13:44:08 -0500

    Type classes cleaned up and simplified.

--- Language/KURE/Combinators.hs
+++ Language/KURE/Combinators.hs
@@ -14,33 +14,34 @@
 -- and the 'Translate' functions operate with 'Rewrite'.
 
 module Language.KURE.Combinators
-	(  -- * The 'Translate' combinators
-	  (<+)
-	, (>->)
-	, failT
-	, readerT
-	, liftT
-	, constT
-	, concatT
+        (  -- * The 'Translate' combinators
+          (<+)
+        , (>->)
+        , failT
+        , readerT
+        , liftT
+        , constT
+        , concatT
         , emptyT
-	  -- * The 'Rewrite' combinators
-	, idR
-	, tryR
-	, repeatR
-	, acceptR
-	, (?)
-	  -- * The 'Walker' combinators
-	, extractR
-	, promoteR
-	, extractT
+        , contextT
+          -- * The 'Rewrite' combinators
+        , idR
+        , tryR
+        , repeatR
+        , acceptR
+        , (?)
+          -- * The 'Walker' combinators
+        , extractR
+        , promoteR
+        , extractT
         , promoteT
-	, topdownR
-	, bottomupR
-	, alltdR
-	, downupR
-	, innermostR
-	, foldT
-    )  where
+        , topdownR
+        , bottomupR
+        , alltdR
+        , downupR
+        , innermostR
+        , foldT
+) where
 
 import Language.KURE.Types
 
@@ -51,14 +52,14 @@ import Data.Monoid
 
 
 infixl 3 <+, >->
-infixr 3 ?
+-- infixr 3 ?
 
 --------------------------------------------------------------------------------
 -- The Translate combinators.
 
 -- | like a catch, '<+' does the first 'Translate', and if it fails, then does the second 'Translate'.
-(<+) :: MonadCatch m => Translate c m a b -> Translate c m a b -> Translate c m a b
-t1 <+ t2 = translate $ \ ca -> apply t1 ca `catchM` (\ _ -> apply t2 ca)
+(<+) :: MonadPlus m => Translate c m a b -> Translate c m a b -> Translate c m a b
+t1 <+ t2 = translate $ \ ca -> apply t1 ca `mplus` apply t2 ca
 
 -- | sequencing translates, the first must be a rewrite for the context to remain valid.
 (>->) :: (EndoFunctor c, Monad m) => Rewrite c m a -> Translate c m a b -> Translate c m a b
@@ -90,6 +91,9 @@ concatT ts = translate (liftM mconcat . forM ts . flip apply)
 emptyT :: (Pointed m, Monoid b) => Translate c m a b
 emptyT = constT mempty
 
+contextT :: Pointed m => Translate c m a (c a)
+contextT = translate point
+
 --------------------------------------------------------------------------------
 
 -- | identity rewrite.
@@ -97,11 +101,11 @@ idR :: (Copointed c, Pointed m) => Rewrite c m a
 idR = rewrite (point . copoint)
 
 -- | catch a failing 'Rewrite', making it into an identity.
-tryR :: (Copointed c, MonadCatch m) => Rewrite c m a -> Rewrite c m a
+tryR :: (Copointed c, Pointed m, MonadPlus m) => Rewrite c m a -> Rewrite c m a
 tryR s = s <+ idR
 
 -- | repeat a rewrite until it fails, then return the result before the failure.
-repeatR :: (EndoCopointed c, MonadCatch m) => Rewrite c m a -> Rewrite c m a
+repeatR :: (EndoFunctor c, Copointed c, Pointed m, MonadPlus m) => Rewrite c m a -> Rewrite c m a
 repeatR s = tryR (s >-> repeatR s)
 
 -- | look at the argument to a 'Rewrite', and choose to be either a failure or trivial success.
@@ -116,46 +120,46 @@ True  ? t  = t
 --------------------------------------------------------------------------------
 
 -- | 'extractT' converts a 'Translate' taking a 'Generic' into a translate over a specific expression type.
-extractT :: (Walker c m a) => Translate c m (Generic a) b -> Translate c m a b
+extractT :: (InjectiveFunctor c, Term a) => Translate c m (Generic a) b -> Translate c m a b
 extractT t = translate (apply t . injectC)
 
 -- | 'promoteT' promotes a 'Translate' into a 'Generic' 'Translate'; other types inside Generic cause failure.
-promoteT  :: Walker c m a => Translate c m a b -> Translate c m (Generic a) b
-promoteT t = translate (maybe (fail "promote") (apply t) . selectC)
+promoteT  :: (InjectiveFunctor c, Monad m, Term a) => Translate c m a b -> Translate c m (Generic a) b
+promoteT t = translate (maybe (fail "promote") (apply t) . retractC)
 
 -- | 'extractR' converts a 'Rewrite' over a 'Generic' into a rewrite over a specific expression type.
-extractR :: Walker c m a => Rewrite c m (Generic a) -> Rewrite c m a
-extractR r = extractT r >>= maybe (fail "extractR") return . select
+extractR :: (InjectiveFunctor c, Monad m, Term a) => Rewrite c m (Generic a) -> Rewrite c m a
+extractR r = extractT r >>= maybe (fail "extractR") return . retract
   
 -- | 'promoteR' promotes a 'Rewrite' into a 'Generic' 'Rewrite'; other types inside Generic cause failure.
 --   'try' can be used to convert a failure-by-default promoteR into a 'id-by-default' promotion.
-promoteR  :: Walker c m a => Rewrite c m a -> Rewrite c m (Generic a)
+promoteR  :: (InjectiveFunctor c, Monad m, Term a) => Rewrite c m a -> Rewrite c m (Generic a)
 promoteR = liftM inject . promoteT
 
 -------------------------------------------------------------------------------
 
 -- | apply a 'Rewrite' in a top down manner.
-topdownR :: (a ~ Generic a, Walker c m a) => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
+topdownR :: Walker c m a => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
 topdownR r = r >-> allR (topdownR r)
 
 -- | apply a 'Rewrite' in a bottom up manner.
-bottomupR :: (a ~ Generic a, Walker c m a) => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
+bottomupR :: Walker c m a => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
 bottomupR r = allR (bottomupR r) >-> r
 
 -- | apply a 'Rewrite' in a top down manner, prunning at successful rewrites.
-alltdR :: (a ~ Generic a, Walker c m a, MonadCatch m) => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
+alltdR :: Walker c m a => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
 alltdR r = r <+ allR (alltdR r)
 
 -- | apply a 'Rewrite' twice, in a topdown and bottom up way, using one single tree traversal.
-downupR :: (a ~ Generic a, Walker c m a) => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
+downupR :: Walker c m a => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
 downupR r = r >-> allR (downupR r) >-> r
 
 -- | a fixed point traveral, starting with the innermost term.
-innermostR :: (a ~ Generic a, Walker c m a, Copointed c, MonadCatch m) => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
+innermostR :: Walker c m a => Rewrite c m (Generic a) -> Rewrite c m (Generic a)
 innermostR r = bottomupR (tryR (r >-> innermostR r))
 
 -- | fold a tree using a single 'Translate' for each node.
-foldT :: (a ~ Generic a, Walker c m a, Monoid b) => Translate c m (Generic a) b -> Translate c m (Generic a) b
+foldT :: (Walker c m a, Monoid b) => Translate c m (Generic a) b -> Translate c m (Generic a) b
 foldT t = concatT [ t, crushT (foldT t) ]
 
 -------------------------------------------------------------------------------
--- Language/KURE/Types.hs
+++ Language/KURE/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
 -- |
 -- Module: Language.KURE.Types
 -- Copyright: (c) 2012 The University of Kansas
@@ -12,10 +12,6 @@
 
 module Language.KURE.Types where
 
-import Prelude hiding (catch)
-import System.IO.Error hiding (catch)
-import Control.Exception (catch)
-
 import Control.Applicative
 import Control.Monad
 import Data.Pointed
@@ -27,9 +23,18 @@ import Data.Monoid
 -- | 'Translate' is a translation or strategy that translates from a value in a context to a monadic value.
 data Translate c m a b = Translate {apply :: c a -> m b}
 
+-- | A 'Rewrite' is a 'Translate' that shares the same source and target type.
+type Rewrite c m a = Translate c m a a
+
 -- | 'translate' is the standard way of building a 'Translate'.
 translate :: (c a -> m b) -> Translate c m a b
 translate = Translate
+
+-- | 'rewrite' is our primitive way of building a 'Rewrite'.
+rewrite :: (c a -> m a) -> Rewrite c m a
+rewrite = translate
+  
+------------------------------------------------------------------------------------------
 
 instance Functor m => Functor (Translate c m a) where
   
@@ -52,8 +57,8 @@ instance Monad m => Monad (Translate c m a) where
   
 -- return :: b -> Translate c m a b
    return b = translate (\ _ -> return b)
--- (>>=) :: Translate c m a b -> (b -> Translate c m a d) -> Translate c m a d  
    
+-- (>>=) :: Translate c m a b -> (b -> Translate c m a d) -> Translate c m a d     
    tb >>= f = translate $ \ ca -> do b <- apply tb ca 
                                      apply (f b) ca
                                      
@@ -61,47 +66,42 @@ instance Monad m => Monad (Translate c m a) where
    fail msg = translate (\ _ -> fail msg)
 
 instance MonadPlus m => MonadPlus (Translate c m a) where
+
 -- mzero :: Translate c m a b  
    mzero = translate (\ _ -> mzero)
 
 -- mplus :: Translate c m a b -> Translate c m a b -> Translate c m a b
    mplus t1 t2 = translate (\ ca -> apply t1 ca `mplus` apply t2 ca)
 
--- | 'Pointed' should be a superclass of 'Applicative', 
---   and 'Applpicative' should be a superclass of 'Monad'.
---   They aren't, but we can make them superclasses of 'MonadCatch'.
-class (Pointed m, Applicative m, Monad m) => MonadCatch m where
--- | Catch monadic 'fail's  
-   catchM :: m a -> (String -> m a) -> m a
+-- We could use MonadError instead of MonadPlus, which would make KURE more powerful
+-- However, that would introduce a dependency on the mtl package
+--   
+-- instance MonadError e m => MonadError e (Translate c m a) where
+-- -- throwError :: e -> Translate c m a b   
+--    throwError e = translate (\ _ -> throwError e)
 
-instance MonadCatch m => MonadCatch (Translate c m a) where
--- catchM :: Translate c m a b -> (String -> Translate c m a) -> Translate c m a
-   catchM t f = translate (\ ca -> apply t ca `catchM` (\ msg -> apply (f msg) ca))
+-- -- catchError :: Translate c m a b -> (e -> Translate c m a b) -> Translate c m a b
+--    catchError t f = translate (\ ca -> apply t ca `catchError` (\ e -> apply (f e) ca))
+               
+------------------------------------------------------------------------------------------
+
+class Injection a b where
+  inject  :: a -> b  
+  retract :: b -> Maybe a
+  -- Law:  retract (inject a) == Just a
   
-instance MonadCatch Maybe where
--- catchM :: Maybe a -> (String -> Maybe a) -> Maybe a
-   catchM (Just a)  _ = Just a  
-   catchM (Nothing) f = f "Failure inside Maybe Monad"
-
-instance MonadCatch (Either String) where
--- catchM :: Either String a -> (String -> Either String a) -> Either String a
-   catchM (Right a)  _ = Right a  
-   catchM (Left msg) f = f msg
-
-instance MonadCatch IO where
--- catchM :: IO a -> (String -> IO a) -> IO a  
-   catchM ma f = ma `catch` (\ e -> if isUserError e
-	 	                     then f $! (ioeGetErrorString e)
-		                     else ioError e)
-                 
--- | 'EndoFunctor' is a Functor that only allows the mapping of endofunctions
+-- | 'EndoFunctor' here is a 'Functor' that only allows the mapping of endofunctions.
 --   This is different from the categorical notion of an endofunctor, 
 --   which is a functor from a category to itself (and which all 'Functor's in Haskell are anyway).   
 class EndoFunctor c where   
   liftC :: (a -> a) -> c a -> c a
+  
+-- | 'InjectiveFunctor' is a 'Functor' that only allows the mapping of injective functions.
+class InjectiveFunctor c where
+  injectC  :: Injection a b => c a -> c b
+  retractC :: Injection a b => c b -> Maybe (c a)
 
-class (Copointed c, EndoFunctor c) => EndoCopointed c where
-
+-- | Replace the value in the context while leaving the context unchanged
 constC :: EndoFunctor c => a -> c a -> c a
 constC a = liftC (const a) 
 
@@ -115,45 +115,22 @@ lowerC f = f . copoint
 lowerC2 :: Copointed c => (a -> b -> d) -> c a -> c b -> d
 lowerC2 f ca cb = f (copoint ca) (copoint cb)
 
-------------------------------------------------------------------------------------------
+class (Copointed c, EndoFunctor c, InjectiveFunctor c) => Context c where
 
--- | A 'Rewrite' is a 'Translate' that shares the same source and target type.
-type Rewrite c m a = Translate c m a a
-
--- | 'rewrite' is our primitive way of building a Rewrite
-rewrite :: (c a -> m a) -> Rewrite c m a
-rewrite = translate
-  
 ------------------------------------------------------------------------------------------
 
 -- | 'Term's are things that syntax are built from.
-class (Generic a ~ Generic (Generic a)) => Term a where
+class (Injection a (Generic a), Generic a ~ Generic (Generic a)) => Term a where
   
   -- | 'Generic' is a sum of all the interesting sub-types, transitively, of @a@.
   -- We use @Generic a ~ a@ to signify that something is its own Generic.
   -- Simple expression types might be their own sole 'Generic', more complex examples
   -- will have a new datatype for the 'Generic', which will also be an instance of class 'Term'.
   type Generic a
-
-  -- | 'inject' injects an expression into a 'Generic'.
-  inject :: a -> Generic a
-
-  -- | 'select' attempts to extract an expression of the correct type from a 'Generic'.
-  select :: Generic a -> Maybe a
-
-
--- | 'TermC's are 'Term's in a context
-class (EndoFunctor c, Term a) => TermC c a where
-  
--- | 'injectC' allows an expression to be injected into a 'Generic' inside a context.
-  injectC :: c a -> c (Generic a)
-  
-  -- | 'selectC' attempts to extract an expression of the correct type from a 'Generic', inside a context.
-  selectC :: c (Generic a) -> Maybe (c a)
   
 
 -- | 'Walker' captures how we walk over an expression in a context, using a monad m. 
-class (TermC c a, Monad m) => Walker c m a where
+class (Context c, Pointed m, MonadPlus m, Term a, a ~ Generic a) => Walker c m a where
 
   -- | 'allR' applies 'Generic' rewrites to all the interesting children of this node.
   allR :: Rewrite c m (Generic a) -> Rewrite c m a
