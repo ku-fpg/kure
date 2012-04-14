d41bbd292255f65f0e43cf312db1c4b73ecd6afc
Neil Sculthorpe <neil@ittc.ku.edu>
2012-04-13 20:13:49 -0500

    Switched to applicative infix syntax

--- tests/fib/FibKURE.hs
+++ tests/fib/FibKURE.hs
@@ -15,14 +15,14 @@ instance Walker () Maybe Arith where
   
   allR r = rewrite $ \ c e -> case e of
                                  Lit n      ->  pure (Lit n)
-                                 Add e1 e2  ->  liftA2 Add (apply r c e1) (apply r c e2)
-                                 Sub e1 e2  ->  liftA2 Sub (apply r c e1) (apply r c e2)
-                                 Fib e      ->  liftA  Fib (apply r c e)
+                                 Add e1 e2  ->  Add <$> apply r c e1 <*> apply r c e2
+                                 Sub e1 e2  ->  Sub <$> apply r c e1 <*> apply r c e2
+                                 Fib e      ->  Fib <$> apply r c e
                                          
   crushT t = translate $ \ c e -> case e of                     
                                     Lit n      ->  pure mempty
-                                    Add e1 e2  ->  liftA2 mappend (apply t c e1) (apply t c e2)
-                                    Sub e1 e2  ->  liftA2 mappend (apply t c e1) (apply t c e2)
+                                    Add e1 e2  ->  mappend <$> apply t c e1 <*> apply t c e2
+                                    Sub e1 e2  ->  mappend <$> apply t c e1 <*> apply t c e2
                                     Fib e      ->  apply t c e
 
   chooseL n = lens $ \ c e -> case e of
