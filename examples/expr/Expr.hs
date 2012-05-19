d99d9b5921ff93ff125d382ce9c44d533ae8afd0
Neil Sculthorpe <neil@ittc.ku.edu>
2012-05-18 19:51:49 -0500

    Cleaned up Expr example.

--- examples/expr/Expr.hs
+++ examples/expr/Expr.hs
@@ -1,17 +1,31 @@
 module Expr where
 
-data Cmd  = Seq Cmd Cmd | Assign Name Expr
-            deriving (Eq,Show)
-
-data Expr = Var Name | Lit Int | Add Expr Expr | ESeq Cmd Expr
-            deriving (Eq,Show)
+-----------------------------------------------------------------
 
 type Name = String
 
-type Context = [(Name,Expr)]
+data Cmd  = Seq Cmd Cmd | Assign Name Expr
+            deriving Eq
 
--- for simplicity we assume the language does not allow variable shadowing
+data Expr = Var Name | Lit Int | Add Expr Expr | ESeq Cmd Expr
+            deriving Eq
+
+instance Show Cmd where
+  show (Seq c1 c2) = show c1 ++ " ; " ++ show c2
+  show (Assign n e) = n ++ " := " ++ show e
+
+instance Show Expr where
+  show (Var n)     = n
+  show (Lit n)     = show n
+  show (Add e1 e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
+  show (ESeq c e)  = "\n( let " ++ show c ++ "\n   in " ++ show e ++ ")\n"
+
+-----------------------------------------------------------------
+
+type Context = [(Name,Expr)]
 
 updateContext :: Cmd -> Context -> Context
 updateContext (Seq c1 c2)  = updateContext c2 . updateContext c1
 updateContext (Assign v e) = ((v,e):)
+
+-----------------------------------------------------------------
--- examples/expr/ExprExamples.hs
+++ examples/expr/ExprExamples.hs
@@ -3,63 +3,74 @@ module ExprExamples where
 import Control.Applicative
 
 import Language.KURE
-import ExprLanguage
-import ExprKURE
+import Expr
+import ExprKure
 
-type ERewrite a = Rewrite Context Maybe a
+-----------------------------------------------------------------
 
-inlineR :: ERewrite Expr
-inlineR = rewrite $ \ c e -> case e of 
+inlineR :: RewriteE Expr
+inlineR = rewrite $ \ c e -> case e of
                                Var v -> lookup v c
                                _     -> empty
 
-inlineGR :: ERewrite GenericExpr
+inlineGR :: RewriteE GenericExpr
 inlineGR = promoteR inlineR
+
+-----------------------------------------------------------------
 
 expr1 :: Expr
 expr1 = ESeq (Seq (Assign "m" (Lit 7))
                   (Assign "n" (Add (Lit 1) (Lit 2)))
-             ) 
-             (Add (Var "m") 
+             )
+             (Add (Var "m")
                   (Var "n")
              )
 
--- Just (ESeq (Seq (Assign "m" (Lit 7)) 
---                 (Assign "n" (Add (Lit 1) (Lit 2)))
---            ) 
---            (Add (Lit 7) 
---                 (Add (Lit 1) (Lit 2)))
---            )
-test1 :: Maybe Expr
-test1 = apply (extractR (anytdR inlineGR)) [] expr1
+result1 :: Expr
+result1 = ESeq (Seq (Assign "m" (Lit 7))
+                    (Assign "n" (Add (Lit 1) (Lit 2)))
+               )
+               (Add (Lit 7)
+                    (Add (Lit 1) (Lit 2))
+               )
 
+test1 :: Bool
+test1 = apply (extractR (anytdR inlineGR)) [] expr1 == Just result1
 
 expr2 :: Expr
 expr2 = ESeq (Seq (Assign "m" (Lit 7))
                   (Assign "n" (Add (Lit 1) (Lit 2)))
-             ) 
-             (Add (Var "m") 
+             )
+             (Add (Var "m")
                   (Var "x")
              )
-        
--- Just (ESeq (Seq (Assign "m" (Lit 7)) 
---                 (Assign "n" (Add (Lit 1) (Lit 2)))
---            ) 
---            (Add (Lit 7) 
---                 (Var "x"))
---            )
-test2 :: Maybe Expr
-test2 = apply (extractR (anytdR inlineGR)) [] expr2
 
+result2 :: Expr
+result2 = ESeq (Seq (Assign "m" (Lit 7))
+                    (Assign "n" (Add (Lit 1) (Lit 2)))
+               )
+               (Add (Lit 7)
+                    (Var "x")
+               )
+test2 :: Bool
+test2 = apply (extractR (anytdR inlineGR)) [] expr2 == Just result2
 
 expr3 :: Expr
 expr3 = ESeq (Assign "m" (Lit 7)
-             ) 
-             (Add (Var "y") 
+             )
+             (Add (Var "y")
                   (Var "x")
              )
-        
--- Nothing
-test3 :: Maybe Expr
-test3 = apply (extractR (anytdR inlineGR)) [] expr3
 
+test3 :: Bool
+test3 = apply (extractR (anytdR inlineGR)) [] expr3 == Nothing
+
+-----------------------------------------------------------------
+
+checkTests :: Bool
+checkTests = and [ test1
+                 , test2
+                 , test3
+                 ]
+
+-----------------------------------------------------------------
