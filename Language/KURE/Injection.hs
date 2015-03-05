ca81280ed347258dde36b23e31e443e47bee677b
Neil Sculthorpe <neil@ittc.ku.edu>
2015-03-05 22:39:16 +0000

    Updated variable names.

--- Language/KURE/Injection.hs
+++ Language/KURE/Injection.hs
@@ -23,6 +23,7 @@ module Language.KURE.Injection
        , projectT
        , extractT
        , promoteT
+       , projectWithFailMsgT
        , promoteWithFailMsgT
        -- * Rewrite Injections
        , extractR
@@ -42,9 +43,9 @@ import Language.KURE.Transform
 --
 -- > project (inject a) == Just a
 
-class Injection a b where
-  inject  :: a -> b
-  project :: b -> Maybe a
+class Injection a u where
+  inject  :: a -> u
+  project :: u -> Maybe a
 
 -- | There is an identity injection for all types.
 instance Injection a a where
@@ -68,71 +69,71 @@ instance Injection a (Maybe a) where
 -------------------------------------------------------------------------------
 
 -- | Injects a value and lifts it into a 'Monad'.
-injectM :: (Monad m, Injection a g) => a -> m g
+injectM :: (Monad m, Injection a u) => a -> m u
 injectM = return . inject
 {-# INLINE injectM #-}
 
 -- | As 'projectM', but takes a custom error message to use if projection fails.
-projectWithFailMsgM :: (Monad m, Injection a g) => String -> g -> m a
+projectWithFailMsgM :: (Monad m, Injection a u) => String -> u -> m a
 projectWithFailMsgM msg = maybe (fail msg) return . project
 {-# INLINE projectWithFailMsgM #-}
 
 -- | Projects a value and lifts it into a 'Monad', with the possibility of failure.
-projectM :: (Monad m, Injection a g) => g -> m a
+projectM :: (Monad m, Injection a u) => u -> m a
 projectM = projectWithFailMsgM "projectM failed"
 {-# INLINE projectM #-}
 
 -------------------------------------------------------------------------------
 
 -- | Lifted 'inject'.
-injectT :: (Monad m, Injection a g) => Transform c m a g
+injectT :: (Monad m, Injection a u) => Transform c m a u
 injectT = arr inject
 {-# INLINE injectT #-}
 
-projectWithFailMsgT :: (Monad m, Injection a g) => String -> Transform c m g a
+projectWithFailMsgT :: (Monad m, Injection a u) => String -> Transform c m u a
 projectWithFailMsgT = contextfreeT . projectWithFailMsgM
 {-# INLINE projectWithFailMsgT #-}
 
 -- | Lifted 'project', the transformation fails if the projection fails.
-projectT :: (Monad m, Injection a g) => Transform c m g a
+projectT :: (Monad m, Injection a u) => Transform c m u a
 projectT = projectWithFailMsgT "projectT failed"
 {-# INLINE projectT #-}
 
 -- | Convert a transformation over an injected value into a transformation over a non-injected value.
-extractT :: (Monad m, Injection a g) => Transform c m g b -> Transform c m a b
+extractT :: (Monad m, Injection a u) => Transform c m u b -> Transform c m a b
 extractT t = injectT >>> t
 {-# INLINE extractT #-}
 
 -- | As 'promoteT', but takes a custom error message to use if promotion fails.
-promoteWithFailMsgT  :: (Monad m, Injection a g) => String -> Transform c m a b -> Transform c m g b
+promoteWithFailMsgT  :: (Monad m, Injection a u) => String -> Transform c m a b -> Transform c m u b
 promoteWithFailMsgT msg t = projectWithFailMsgT msg >>> t
 {-# INLINE promoteWithFailMsgT #-}
 
 -- | Promote a transformation over a value into a transformation over an injection of that value,
 --   (failing if that injected value cannot be projected).
-promoteT  :: (Monad m, Injection a g) => Transform c m a b -> Transform c m g b
+promoteT  :: (Monad m, Injection a u) => Transform c m a b -> Transform c m u b
 promoteT = promoteWithFailMsgT "promoteT failed"
 {-# INLINE promoteT #-}
 
 -- | As 'extractR', but takes a custom error message to use if extraction fails.
-extractWithFailMsgR :: (Monad m, Injection a g) => String -> Rewrite c m g -> Rewrite c m a
+extractWithFailMsgR :: (Monad m, Injection a u) => String -> Rewrite c m u -> Rewrite c m a
 extractWithFailMsgR msg r = injectT >>> r >>> projectWithFailMsgT msg
 {-# INLINE extractWithFailMsgR #-}
 
 -- | Convert a rewrite over an injected value into a rewrite over a projection of that value,
 --   (failing if that injected value cannot be projected).
-extractR :: (Monad m, Injection a g) => Rewrite c m g -> Rewrite c m a
+extractR :: (Monad m, Injection a u) => Rewrite c m u -> Rewrite c m a
 extractR = extractWithFailMsgR "extractR failed"
 {-# INLINE extractR #-}
 
 -- | As 'promoteR', but takes a custom error message to use if promotion fails.
-promoteWithFailMsgR :: (Monad m, Injection a g) => String -> Rewrite c m a -> Rewrite c m g
+promoteWithFailMsgR :: (Monad m, Injection a u) => String -> Rewrite c m a -> Rewrite c m u
 promoteWithFailMsgR msg r = projectWithFailMsgT msg >>> r >>> injectT
 {-# INLINE promoteWithFailMsgR #-}
 
 -- | Promote a rewrite over a value into a rewrite over an injection of that value,
 --   (failing if that injected value cannot be projected).
-promoteR  :: (Monad m, Injection a g) => Rewrite c m a -> Rewrite c m g
+promoteR  :: (Monad m, Injection a u) => Rewrite c m a -> Rewrite c m u
 promoteR = promoteWithFailMsgR "promoteR failed"
 {-# INLINE promoteR #-}
 
